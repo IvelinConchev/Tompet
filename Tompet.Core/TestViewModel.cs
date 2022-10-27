d72d48a2223c7cb747aa668075eab0906d967aa0
ivelco <ivelin_conchev@abv.bg>
2022-10-27 20:40:50 +0300

    Add project files.

---
+++ Tompet.Core/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Tompet.Core
+{
+    using System;
+    using Tompet.Core.CustomAttributes;
+
+    public class TestViewModel
+    {
+        public DateTime FirstDate { get; set; }
+
+        [IsBefore(nameof(FirstDate), errorMessage: "Boom")]
+        public DateTime SecondDate { get; set; }
+    }
+}
---
+++ Tompet/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace Tompet.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+    using Tompet.Core.Constants;
+    using Tompet.Models;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.SuccessMessage] = "Браво, успяхме да подкараме тостера!";
+            return View();
+        }
+
+        public IActionResult Privacy(ErrorViewModel error)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
